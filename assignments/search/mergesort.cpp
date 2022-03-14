ff4bb89fa2bcc19cd8320f6d56c0fcaf994e7d67
RBT0101 <zhipeng.lin63@myhunter.cuny.edu>
2022-03-14 09:46:58 -0400

    Created mergesort.cpp file

---
+++ assignments/search/mergesort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+	return 0;
+}
+
+std::vector<int> mergeSort(vector<int> &unorderedList){
+	if (unorderedList.size() <= 0) return vector<int> {};
+	else if (unorderedList.size() == 1) return unorderedList;
+	else{
+		int mid = unorderedList.size() / 2;
+		vector<int> left = ;
+		vector<int> right = ;
+	}	
+}
+
+std::vector<int> merge(&vector<int> left, &vector<int> right){
+	vector<int> orderedList;
+	int maxSize;
+
+	while (maxSize > 0){
+		//Check both 
+		//Check left
+		//Check right
+	}
+
+	//return orderedList
+}
+
+
